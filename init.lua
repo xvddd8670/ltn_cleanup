fd67121a559ce772b2d089188a18dc7561449318
unknown <xvddd8670@gmail.com>
2025-07-04 13:50:46 +0200

    fixed multiplayer

--- init.lua
+++ init.lua
@@ -1,9 +1,17 @@
 local handler = require("event_handler")
 
+local function register_event()
+  if storage.ltn_event_id then
+    script.on_event(storage.ltn_event_id, handler.handle_event)
+  end
+end
+
 local function on_init_or_configuration_changed()
   local event_id = remote.call("logistic-train-network", "on_requester_remaining_cargo")
-  script.on_event(event_id, handler.handle_event)
+  storage.ltn_event_id = event_id
+  register_event()
 end
 
 script.on_init(on_init_or_configuration_changed)
 script.on_configuration_changed(on_init_or_configuration_changed)
+script.on_load(register_event)
--- signals.lua
+++ signals.lua
@@ -2,7 +2,7 @@ data:extend({
     {
         type = "virtual-signal",
         name = "ltn-cleanup-station",
-        icon = "__ltn-cleanup-2.0__/graphics/signals/signal_cleanup.png",
+        icon = "__ltn-cleanup-v2__/graphics/signals/signal_cleanup.png",
         icon_size = 64, icon_mipmaps = 4,
         subgroup = "LTN-signal",
         order = "e-a"
@@ -10,7 +10,7 @@ data:extend({
     {
         type = "virtual-signal",
         name = "ltn-item-cleanup-station",
-        icon = "__ltn-cleanup-2.0__/graphics/signals/signal_item_cleanup.png",
+        icon = "__ltn-cleanup-v2__/graphics/signals/signal_item_cleanup.png",
         icon_size = 64, icon_mipmaps = 4,
         subgroup = "LTN-signal",
         order = "e-a"
